0c65ad715cb4579ffa8d22d79aae8d7371affd57
Fridolín Pokorný <fridolin@redhat.com>
2018-12-04 10:09:03 +0100

    Fix syntax error

--- scripts/wait-for-indexes.groovy
+++ scripts/wait-for-indexes.groovy
@@ -23,14 +23,14 @@ indexes = [
 ]
 
 indexes.each { i ->
-   index = mgmt.getGraphIndex(i)
-   propertykeys = index.getFieldKeys()
-   propertykeys.each { j ->
-   indexof = propertykeys.findIndexOf{it ==~ j}
-   indexcurrentstatus = index.getIndexStatus(propertykeys[indexof])
-   if (indexcurrentstatus == SchemaStatus.REGISTERED)
-   {
-     System.err.println "Schema Status is in REGISTERED mode"
-     mgmt.awaitGraphIndexStatus(graph, i).status(SchemaStatus.ENABLED).call()
-   }
+  index = mgmt.getGraphIndex(i)
+  propertykeys = index.getFieldKeys()
+  propertykeys.each { j ->
+    indexof = propertykeys.findIndexOf{it ==~ j}
+    indexcurrentstatus = index.getIndexStatus(propertykeys[indexof])
+    if (indexcurrentstatus == SchemaStatus.REGISTERED) {
+      System.err.println "Schema Status is in REGISTERED mode"
+      mgmt.awaitGraphIndexStatus(graph, i).status(SchemaStatus.ENABLED).call()
+    }
+  }
 }
